5cf965b7f25ea379bb2c7482497a41c9b5a34870
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-20 10:34:48 -0500

    Added weather app api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+
+const API_KEY = `${process.env.REACT_APP_WEATHER_API_KEY}`;
+
+
 class App extends React.Component{
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value
+
+        var country = e.target.elements.country.value;
+
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`)
+        //Convert the api data to Json format to a readable format
+        const data = await api_call.json();
+        console.log(data)
+    }
     render() {
         return (
             <div>
                 <Titles/>
-                <Form/>
+                {/* Giving Form component access to the getWeather function */}
+                <Form getWeather={this.getWeather}/>
                 <Weather />
 
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,12 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-        <p>Form Component</p>
+          {/* On form submission use getWeather function passed in via props from App.js */}
+    <form onSubmit={this.props.getWeather}>
+        <input type="text" name="city" placeholder="city.."/>
+        <input type="text" name="country" placeholder="county.."/>
+        <button> Get Weather</button>
+    </form>
       </div>
     )
   }
