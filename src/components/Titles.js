f726ecb59cdeaa8c606284222695a16378e808d7
Shante Austin <shanteaustin@Shantes-MacBook-Air.local>
2019-01-20 08:33:37 -0500

    setup

---
+++ src/components/Titles.js
@@ -0,0 +1,17 @@
+import React  from 'react'
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+          <div>
+              <h1>Weather Finder</h1>
+              <p>Find out Tempature, Conditions, and More</p>
+          </div>
+        
+      </div>
+    )
+  }
+}
+
+export default Titles;
